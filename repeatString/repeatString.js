8361005005ac076402ddb06be25f821f331b36c7
Pedro Silva <pedro.revez.silva@gmail.com>
2018-10-27 15:02:15 +0100

    Do repeatString exercise

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, count) {
+    if(count<0) return 'ERROR';
+    var fullString = '';
+    for( i=0 ; i<count ; i++){
+        fullString += string;
+    }
+    return fullString;
 }
 
 module.exports = repeatString
