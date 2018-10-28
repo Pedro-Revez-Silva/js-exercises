e0c036151fb4321fe578e063d86506cc81495160
Pedro Silva <pedro.revez.silva@gmail.com>
2018-10-28 13:31:39 +0000

    Update removeFromArray.js

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+   let originalArray = arguments[0];
+  for(let i = 1 ; i < arguments.length ; i++){
+    for(let i2 = 0 ; i2 < originalArray.length ; i2++){
+      if(originalArray[i2] == arguments[i]){
+        originalArray.splice(i2, 1);
+      }
+    }
+  }
+  return originalArray;
 }
 
 module.exports = removeFromArray
