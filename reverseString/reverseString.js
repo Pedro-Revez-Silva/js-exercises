aa0547224eedcdfa4695cd481a4451b187d6ec9e
pedro.revez.silva <pedro.revez.silva@gmail.com>
2018-10-28 07:54:06 -0400

    Do reverseString exercise

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+  let outputString = '';
+  let counter = inputString.length - 1;
+  
+  for(;counter >= 0; counter--){
+    outputString += inputString[counter];
+  }
+  return outputString;
 }
 
-module.exports = reverseString
+module.exports = reverseString;
