3087ee2ee32c7e60e53c9cde0e9d102fe401dda7
pedro.revez.silva <pedro.revez.silva@gmail.com>
2018-10-29 10:11:40 -0400

    Do TempConversion - Final & Tested

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  let convertedTemp = (temperature-32)*(5/9);
+  return parseFloat(convertedTemp.toFixed(1), 10);
 }
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  let convertedTemp = temperature*(9/5)+32;
+  return parseFloat(convertedTemp.toFixed(1), 10);
 }
 
 module.exports = {
